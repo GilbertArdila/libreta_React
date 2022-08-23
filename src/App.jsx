1b6a315e506449b1ab435e11fbda08f00a02f358
Gilbert Ardila <gilbertferney@gmail.com>
2022-08-23 11:08:06 -0500

    iniciamos proyecto libreta con tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {Header} from './Components/Header'
+import {Form} from './Components/Form'
+import {Lista} from './Components/Lista'
+
+
+
+function App() {
+ 
+
+  return (
+    <div className='container mx-auto bg-blue-100'>
+      <Header></Header>
+      <Form></Form>
+      <Lista></Lista>
+    </div>
+  )
+}
+
+export {App} 
